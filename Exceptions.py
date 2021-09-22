ef6a280e7b86708b8ed0244e031d0ee08e44ff13
Ihor <66252420+Grigoryan-not-available@users.noreply.github.com>
2021-09-22 09:21:02 +0300

    Add files via upload

--- Exceptions.py
+++ Exceptions.py
@@ -12,3 +12,23 @@ def catcher(func, obj, *args):
 s = 'spam'
 catcher(fetcher, s, 5)
 
+def kaboom(x, y):
+    return x + y
+
+try:
+    print(kaboom(['1', '2', '3'], 1))
+except TypeError:
+    print('Type Error')
+print('resuming')
+
+class MyError(Exception): pass
+
+def f(x):
+    assert x > 0, 'test x < 0'
+    return x ** 2
+
+print(f(3), '\n')
+
+with open('Corp_Site_Sclad.png', 'rb') as file1, open('cp_Corp_Site_Sclad.png', 'wb') as file2:
+    file2.write(file1.read())
+
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-import Exceptions
-
 #exec(open('script1.py').read())
+from datetime import datetime
+
+def timeit(name):
+    print(name)
+    def outer(func):
+        def wrapper(*args, **kwargs):
+            start = datetime.now()
+            result = func(*args, **kwargs)
+            print(datetime.now() - start)
+            return result
+        return wrapper
+    return outer
+
+@timeit('kek')
+def loopGenlist(n):
+    lists = []
+    for i in range(n):
+        if i % 2 == 0:
+            lists.append(i)
+    return lists
+
+@timeit('lol')
+def listGen(n):
+    lists = [i for i in range(n) if i % 2 == 0]
+    return lists
+
+# f1loop = loopGenlist(10**5)
+# f2glisten = listGen(10**5)
