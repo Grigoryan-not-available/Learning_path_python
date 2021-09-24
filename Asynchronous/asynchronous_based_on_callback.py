76b2eed21c70a080d92df76b041dda52aaddff0e
Ihor <66252420+Grigoryan-not-available@users.noreply.github.com>
2021-09-24 22:48:02 +0300

    Add files via upload

---
+++ Asynchronous/asynchronous_based_on_callback.py
@@ -0,0 +1,38 @@
+import socket
+import selectors
+
+selector = selectors.DefaultSelector()
+
+def server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('localhost', 5000))
+    server_socket.listen()
+    selector.register(fileobj=server_socket, events=selectors.EVENT_READ, data=accept_connections)
+
+def accept_connections(server_socket):
+        client_socket, addr = server_socket.accept()
+        print(f'{addr} successfully connect')
+        selector.register(fileobj=client_socket, events=selectors.EVENT_READ, data=send_msg)
+
+def send_msg(client_socket):
+        request = client_socket.recv(4096)
+        if request:
+            response = ('Hello world\n').encode() + request
+            client_socket.send(response)
+        else:
+            selector.unregister(client_socket)
+            client_socket.close()
+
+def event_loop():
+    while True:
+        events = selector.select()
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj)
+
+
+
+if __name__ == '__main__':
+    server()
+    event_loop()
